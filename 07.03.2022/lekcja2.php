93325c86e6749066c37781a42f75596d409d0441
Szarafiński <100858951+McKimo2003@users.noreply.github.com>
2022-03-14 08:06:17 +0100

    Add files via upload

--- 07.03.2022/lekcja2.php
+++ 07.03.2022/lekcja2.php
@@ -3,7 +3,33 @@
     ZSŁ - Zespół Szkół
     Łączności
 SCHOOL;
-echo "$text<hr>";
-echo nl2br($text);
-echo "<hr>";
+    echo "$text<hr>";
+    echo nl2br($text);
+    echo "<hr>";
+
+    $name="JaNuSz";
+    echo $name."<br>";
+    echo strtolower($name)."<br>";
+    echo strtoupper($name)."<br>";
+    $surname="Bąk";
+    echo $surname."<br>";
+    echo strtolower($surname)."<br>";
+    echo strtoupper($surname)."<br>";
+    echo mb_strtoupper($surname)."<hr>";
+    $text="Jakiś kowalski";
+    echo $text."<br>";
+    echo mb_strtolower($text)."<br>";
+    echo ucfirst(mb_strtolower($text))."<br>";
+    echo ucwords(mb_strtolower($text))."<hr>";
+    $Lorem="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed venenatis mi vitae odio lacinia, et fringilla felis cursus. Aliquam eleifend auctor condimentum. Cras fermentum sit amet libero ac ultricies. Quisque hendrerit placerat mi, at tempus lectus interdum vitae. Sed enim odio, hendrerit eget dolor vel, ultrices malesuada est. Praesent bibendum placerat rutrum. Etiam et massa quis nibh pretium egestas in id dui. Aenean vulputate nulla sed dolor imperdiet vestibulum. Integer maximus imperdiet leo. Proin non accumsan turpis. Fusce egestas orci non est blandit consequat. Nullam ac ipsum et nunc suscipit dapibus ut nec magna. Vivamus non posuere velit, et condimentum ante. Maecenas eget quam convallis, dignissim dolor eu, laoreet mi. Vivamus id quam rutrum, hendrerit risus eu, facilisis mauris.Morbi a aliquet est, at viverra nisl. Proin at ornare libero, aliquam gravida quam. Donec ullamcorper, ex a accumsan accumsan, lectus velit cursus odio, ut pretium odio purus eu augue. Donec arcu urna, suscipit at egestas eu, facilisis et nulla. Morbi sed nisl non orci rutrum commodo at vitae orci. In id varius turpis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas id nibh elementum, gravida nisi eget, imperdiet orci.";
+    echo $Lorem."<hr>";
+    echo wordwrap($Lorem, 50, "<br>");
+    ob_clean();
+    $name="  jan ";
+    echo $name,"<br>";
+    echo "Długość imienia $name wynosi: ".strlen($name)."<br>";
+    echo "Długość imienia ".ltrim($name)." wynosi: ".strlen(ltrim($name))."<br>"; //Usuwa lewa
+    echo "Długość imienia ".rtrim($name)." wynosi: ".strlen(rtrim($name))."<br>"; //Usuwa Prawa
+    echo "Długość imienia ".trim($name)." wynosi: ".strlen(trim($name))."<br>";
+    
 ?>
